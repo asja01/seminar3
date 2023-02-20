e854a5824597c0090036c5f67a0e301ec9d5d558
Nastya <asja01@rambler.ru>
2023-02-20 15:21:46 +0300

    sem3

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+int InputNumber(string str)
+{
+    Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x1 = InputNumber("Введите координту х1: ");
+int y1 = InputNumber("Введите координту y1: ");
+int z1 = InputNumber("Введите координту z1: ");
+
+int x2 = InputNumber("Введите координту х2: ");
+int y2 = InputNumber("Введите координту y2: ");
+int z2 = InputNumber("Введите координту z2: ");
+
+Console.WriteLine(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1),2)));
+
---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+void PrintSquares(int x)
+{
+    for (int i = 1; i <= x; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+    }
+}
+Console.WriteLine("Введите число N");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+PrintSquares(a);
